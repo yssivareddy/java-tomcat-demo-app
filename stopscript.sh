3308f43201549d14c07f25338ef8a1c5f1dc361f
rajnikhattarrsinha <36462792+rajnikhattarrsinha@users.noreply.github.com>
2019-04-09 16:30:00 +0530

    Update stopscript.sh

--- stopscript.sh
+++ stopscript.sh
@@ -2,7 +2,7 @@
 
 docker stop $(docker ps -a -q)
 
-#BUSY_PORT=$(sudo fuser 8080/tcp)
+#BUSY_PORT=$(sudo fuser 8082/tcp)
 #if [ ! -z "$BUSY_PORT" ]; then
- #  sudo fuser -k 8080/tcp
+ #  sudo fuser -k 8082/tcp
 #fi
